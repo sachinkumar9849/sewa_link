19fbc06f1cb3f14e3cddd5400976f04e64816cf9
sachin kumar jha <kjhasachin@gmail.com>
2022-01-21 17:50:35 +0545

    21-1-2021-sachin

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -45,6 +45,53 @@ $('.slider_banner').slick({
     ]
 });
 // Banner Slick Slider Ends
+// Three banner slider Starts
+// $('.three_banner_slider').slick({
+//     autoplay: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     arrows: true,
+//     dots: false,
+//     responsive: [{
+//             breakpoint: 1400,
+//             settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 1
+//             }
+//         },
+//         {
+//             breakpoint: 1080,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 1
+//             }
+//         },
+//         {
+//             breakpoint: 780,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 1
+//             }
+//         },
+//         {
+//             breakpoint: 600,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1,
+//                 dots: true
+//             }
+//         },
+//         {
+//             breakpoint: 480,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1,
+//                 dots: true
+//             }
+//         }
+//     ]
+// });
+// Three banner slider Ends
 
 // Recommendation For You start
 
@@ -102,44 +149,7 @@ $('.flash_men').slick({
     slidesToScroll: 1,
     arrows: false,
     dots: false,
-    responsive: [{
-            breakpoint: 1400,
-            settings: {
-                slidesToShow: 4,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 1080,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 780,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                dots: true
-            }
-        }
-    ]
+   
 });
 
 // Flash end
@@ -200,16 +210,16 @@ $('.best_selling').slick({
     arrows: true,
     dots: false,
     responsive: [{
-            breakpoint: 1400,
+            breakpoint: 1444,
             settings: {
-                slidesToShow: 4,
+                slidesToShow: 3,
                 slidesToScroll: 1
             }
         },
         {
             breakpoint: 1080,
             settings: {
-                slidesToShow: 1,
+                slidesToShow: 2,
                 slidesToScroll: 1
             }
         },
@@ -286,6 +296,52 @@ $('.best_selling').slick({
         }, 0)
     }());
 // Countdown end
+
+// Countdown Two start
+(function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+  
+    //I'm adding this section so I don't have to keep updating this pen every year :-)
+    //remove this if you don't need it
+    let today = new Date(),
+        dd = String(today.getDate()).padStart(2, "0"),
+        mm = String(today.getMonth() + 1).padStart(2, "0"),
+        yyyy = today.getFullYear(),
+        nextYear = yyyy + 1,
+        dayMonth = "09/30/",
+        birthday = dayMonth + yyyy;
+    
+    today = mm + "/" + dd + "/" + yyyy;
+    if (today > birthday) {
+      birthday = dayMonth + nextYear;
+    }
+    //end
+    
+    const countDown = new Date(birthday).getTime(),
+        x = setInterval(function() {    
+  
+          const now = new Date().getTime(),
+                distance = countDown - now;
+  
+        //   document.getElementById("days").innerText = Math.floor(distance / (day)),
+            document.getElementById("hours_a").innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById("minutes_a").innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById("seconds_a").innerText = Math.floor((distance % (minute)) / second);
+  
+          //do something later when date is reached
+          if (distance < 0) {
+            document.getElementById("headline").innerText = "It's my birthday!";
+            document.getElementById("countdown").style.display = "none";
+            document.getElementById("content").style.display = "block";
+            clearInterval(x);
+          }
+          //seconds
+        }, 0)
+    }());
+// Countdown Two end
 
 // Delivery info search start
 //https://codepen.io/saravanajd/pen/GGPQbY?editors=0010
